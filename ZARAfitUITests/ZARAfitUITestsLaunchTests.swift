53b38116bf55f3b2c7224de11a3cb8b50af09c4d
david oriz <david.oriz.developer>
2023-09-22 12:22:56 -0500

    project created

---
+++ ZARAfitUITests/ZARAfitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ZARAfitUITestsLaunchTests.swift
+//  ZARAfitUITests
+//
+//  Created by Davit on 22/09/23.
+//
+
+import XCTest
+
+final class ZARAfitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
