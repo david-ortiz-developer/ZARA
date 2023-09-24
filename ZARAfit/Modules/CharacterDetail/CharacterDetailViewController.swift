7d6910ac9fff49b66f0f57fb401bdf195164ebfb
david oriz <david.oriz.developer>
2023-09-24 14:57:45 -0500

    Integrated image and name label to the detail

--- ZARAfit/Modules/CharacterDetail/CharacterDetailViewController.swift
+++ ZARAfit/Modules/CharacterDetail/CharacterDetailViewController.swift
@@ -6,9 +6,43 @@
 //
 
 import UIKit
+import Lottie
+
 class CharacterDetailViewController: UIViewController {
     var character: CharacterObject!
+    @IBOutlet weak var characterImage: ImageLoader!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var characterName: UILabel!
+    private var closeAnimationView: LottieAnimationView?
+    private var frameAnimationView: LottieAnimationView?
+    
     override func viewDidLoad() {
         assert(character != nil, "Character can't be nil")
+        if let strUrl = character.image.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed),
+           let imgUrl = URL(string: strUrl) {
+            characterImage.loadImageWithUrl(imgUrl)
+            characterImage.layer.cornerRadius = 15
+        }
+        closeAnimationView = .init(name:"animation_lmxtsihn")
+        closeAnimationView?.frame = closeButton.frame
+        closeAnimationView?.contentMode = .scaleAspectFit
+        closeAnimationView?.loopMode = .autoReverse
+        self.view.addSubview(closeAnimationView!)
+        closeAnimationView?.play()
+        
+        frameAnimationView = .init(name:"animation_lmxum64d")
+        frameAnimationView?.frame = characterImage.frame
+        frameAnimationView?.contentMode = .scaleAspectFit
+        frameAnimationView?.loopMode = .autoReverse
+        self.view.addSubview(frameAnimationView!)
+        frameAnimationView?.play()
+        
+        self.characterName.text = character.name
+        
+        self.view.bringSubviewToFront(closeButton)
+        
+    }
+    @IBAction func closeView(sender: UIButton) {
+        self.dismiss(animated: true)
     }
 }
